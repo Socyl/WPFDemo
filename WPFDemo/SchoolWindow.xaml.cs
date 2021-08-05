6bec174effb56bc9c56f9d616fa0155e188dcb91
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-08-04 22:17:08 -0600

    Add project files.

---
+++ WPFDemo/SchoolWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for SchoolWindow.xaml
+    /// </summary>
+    public partial class SchoolWindow : Window
+    {
+        private School school;
+        public SchoolWindow()
+        {
+            InitializeComponent();
+            school = new School();
+            lbCampuses.ItemsSource = school.Campuses;
+            lbCourses.ItemsSource = school.Courses;
+            lbMajors.ItemsSource = school.Majors;
+            lbStudents.ItemsSource = school.Students;
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Student newStudent = new Student();
+            StudentWindow studentWindow = new StudentWindow(newStudent);
+            studentWindow.ShowDialog();
+            if (studentWindow.DialogResult == true)
+            {
+                MessageBox.Show("Student "+newStudent.StudentNumber+" Added!");
+                school.Students.Add(newStudent);
+                lbStudents.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Dialog Cancelled!");
+            }
+        }
+
+        private void bntSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text Files|*.txt";
+
+            bool? result= dialog.ShowDialog();
+            if (result == true)
+            {
+                string path = dialog.FileName;
+                string s = "";
+                foreach(Student student in school.Students)
+                {
+                    s += student.ToString()+"\n";
+                }
+
+                File.WriteAllText(path, s);
+            }
+
+        }
+    }
+}
---
+++ WPFDemo/StudentWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StudentWindow : Window
+    {
+        public Student TheStudent { get; }
+
+        public StudentWindow():this(new Student())
+        {
+            
+        }
+
+        public StudentWindow(Student newStudent)
+        {
+            InitializeComponent();
+            TheStudent = newStudent;
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+            //Set the values from our controls into the class
+            TheStudent.FirstName = txbFirstName.Text;
+            TheStudent.LastName = txbLastName.Text;
+            TheStudent.StudentNumber = txbStudentNum.Text;
+            TheStudent.Major = txbMajor.Text;
+
+            List<Assignment> scores = new List<Assignment>();
+            foreach (Assignment score in lbScores.Items)
+            {
+                scores.Add(score);    
+            }
+            TheStudent.Scores = scores;
+
+            //Set the results from the class into a control on the form
+            txbResults.Text = TheStudent.ToString();
+            DialogResult = true;
+            Close();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Assignment assign = new Assignment();
+            assign.Title = txbTitle.Text;
+            assign.Score = int.Parse(txbScore.Text);
+            lbScores.Items.Add(assign);
+          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
